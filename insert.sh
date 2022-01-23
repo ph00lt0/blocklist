c4fe78e1aba5a6f5d32cd276516f460107c3c549
ph00lt0 <->
2022-01-23 15:04:28 +0000

    update blocklist

--- insert.sh
+++ insert.sh
@@ -5,4 +5,5 @@ read -p "Enter the domain that you want to add:" domain
 
 if [ "$domain" != "" ]; then
       printf "||$domain^\n" >> './stats-analytics.txt'
+      git commit -am "update blocklist" && pushall
 fi

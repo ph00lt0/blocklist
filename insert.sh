4222252945ce6a55431688f8ce2ce8fb09dbd2ad
ph00lt0 <->
2022-01-23 15:26:09 +0000

    update blocklist

--- insert.sh
+++ insert.sh
@@ -4,13 +4,14 @@
 read -p "Enter the domain that you want to add:" domain
 
 declare file="./stats-analytics.txt"
+declare rule="||$domain^"
 
-if grep -q $domain "$file"; then
-        echo "$domain already present"
+if grep -q $rule "$file"; then
+        echo "$domain rule already present"
     else
         if [ "$domain" != "" ]; then
-            printf "||$domain^\n" >> "$file"
-            # git commit -am "update blocklist" && git push origin master && git push github master
+            printf "$rule\n" >> "$file"
+            git commit -am "update blocklist" && git push origin master && git push github master
             echo "added $domain"
         else
             echo "no domain provided"

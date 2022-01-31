699b20f1b5e98016658de10627c0897bd18fe672
ph00lt0 <->
2022-01-31 00:15:38 +0000

    check existence before pulling

--- insert.sh
+++ insert.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "checking for updates..."
-git pull origin master && git pull github master
-
 read -p "Enter the domain that you want to add:" domain
 
 declare file="./blocklist.txt"
@@ -15,12 +12,19 @@ declare rule="||$domain^"
 if grep -q $rule "$file"; then
         echo "$domain rule already present"
     else
-        if [ "$domain" != "" ]; then
-            printf "$rule\n" >> "$file"
-            git commit -am "added $domain to blocklist" && git push origin master && git push github master
-            echo "added $domain"
-        else
-            echo "no domain provided"
+        echo "checking for updates..."
+        git pull origin master && git pull github master
+        
+        if grep -q $rule "$file"; then
+            echo "$domain rule added before you"
+        else    
+            if [ "$domain" != "" ]; then
+               printf "$rule\n" >> "$file"
+               git commit -am "added $domain to blocklist" && git push origin master && git push github master
+               echo "added $domain"
+            else
+               echo "no domain provided"
+            fi
         fi
 fi
 

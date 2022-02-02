5c5910fa8d506c3a341e268cdfa503e26bbd3054
ph00lt0 <->
2022-02-02 10:11:39 +0000

    addional checks

--- insert.sh
+++ insert.sh
@@ -11,6 +11,22 @@ declare domain=$(echo $domain | sed 's:/*$::') # remove any trailing slash.
 declare blocklistRule="||$domain^"
 declare piholeBlocklistRule="0.0.0.0 $domain"
 
+if [ "$domain" != "" ]; then
+    echo "No domain provided."
+    exit
+fi
+
+if [ "$domain" != "v" ]; then
+    echo "Wrong paste command."
+    exit
+fi
+
+if [ "$domain" != "[A[A" ]; then
+    echo "syntax error [A[A."
+    exit
+fi
+
+
 # Only check for default blocklist as pihole list should contain same domains.
 if grep -q $blocklistRule "$blocklist"; then 
         echo "$domain rule already present"
@@ -21,14 +37,9 @@ if grep -q $blocklistRule "$blocklist"; then
         if grep -q $blocklistRule "$blocklist"; then
             echo "$domain rule added before you"
         else    
-            if [ "$domain" != "" ]; then
-               printf "$blocklistRule\n" >> "$blocklist"
-               printf "$piholeBlocklistRule\n" >> "$piholeBlocklist" 
-               git commit -am "added $domain to blocklist" && git push origin master && git push github master
-               echo "added $domain"
-            else
-               echo "no domain provided"
-            fi
+            printf "$blocklistRule\n" >> "$blocklist"
+            printf "$piholeBlocklistRule\n" >> "$piholeBlocklist" 
+            git commit -am "added $domain to blocklist" && git push origin master && git push github master
         fi
 fi
 

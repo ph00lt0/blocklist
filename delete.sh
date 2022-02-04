7f29b743ecb73218b011e2ee5ca320202f9e8ca2
ph00lt0 <->
2022-02-04 17:23:02 +0000

    add removal script

---
+++ delete.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "This is not for delisting domains, please use ./delist.sh"
+read -p "Enter the mistake entry that you want to delete:" mistake
+
+if [ "$mistake" == "" ]; then
+    echo "No mistake entry provided."
+    exit
+fi
+
+if [ "$mistake" == "v" ]; then
+    echo "Wrong paste command."
+    exit
+fi
+
+if [ "$mistake" == "[A[A" ]; then
+    echo "syntax error [A[A."
+    exit
+fi
+
+declare blocklist="./blocklist.txt"
+declare piholeBlocklist="./pihole-blocklist.txt"
+
+declare mistake=$(echo $mistake | sed -E 's/^\s*.*:\/\///g') # remove any https:// or http://.
+declare mistake=$(echo $mistake | sed 's:/*$::') # remove any trailing slash.
+
+declare author=$(git config user.name)
+
+declare blocklistRule="||$mistake^"
+declare piholeBlocklistRule="0.0.0.0 $mistake"
+
+
+# Only check for default blocklist as pihole list should contain same domains.
+if grep -q $blocklistRule "$blocklist"; then
+    echo "checking for updates..."
+    git pull origin master && git pull github master
+
+    if grep -q $blocklistRule "$blocklist"; then
+        sed -i '' "/$blocklistRule/d" $blocklist
+        sed -i '' "/$piholeBlocklistRule/d" $piholeBlocklist
+        git commit -am "Mea culpa, $author made a mistake: $mistake now removed from blocklist" && git push origin master && git push github master
+    else
+          echo "$domain rule corrected before you"
+    fi
+else
+    echo "$domain mistake not present"
+fi

51801b6a8075a8c3d0de845f29b35c36aebf8c9f
ph00lt0 <->
2022-06-15 12:09:58 +0100

    add remove class rule script

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -45,6 +45,15 @@ Enter the same entry you made by mistake, https:// or http and trailing / are au
 Hit enter, and the mistake will be corrected, the commit message will blame you for it and you can continue your life.
 
 
+# Add rule to remove a class from element
+Do you want to remove a class from an element? For example a class making the body disappear to show a cookie banner?
+
+- Enter the domain name, the class, and the element to remove it from
+
+Hit enter, and the class removal rule will be added
+Note that all rules are set to stay active, so that they will continue to work on page refreshes as well as with async content.
+
+
 # Got an error pushing upstream?
 This is likely due to you commiting just at the same time someone else is pushing upstream. You can use the following command to retry adding a domain rather then fixing the merge conflict manually.
 ```
---
+++ insert-remove-class-rule.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+read -p "Enter the domain that you want to add: " domain
+
+declare blocklist="./blocklist.txt"
+
+declare domain=$(echo $domain | sed -E 's/^\s*.*:\/\///g') # remove any https:// or http://.
+declare domain=$(echo $domain | sed 's:/*$::') # remove any trailing slash.
+
+read -p "Enter the class that you want to remove: " class
+
+read -p "Enter the element the class should be removed from: " elm
+
+declare version=$(date +"%Y%m%d")
+declare versionTag="! Version: "
+declare versionLine="! Version: $version"
+
+declare blocklistRule="${domain}##+js(rc, ${class}, ${elm}, stay)"
+
+if [ "$domain" == "" ]; then
+    echo "No domain provided."
+    exit
+fi
+
+if [ "$domain" == "v" ]; then
+    echo "Wrong paste command."
+    exit
+fi
+
+if [ "$domain" == "[A[A" ]; then
+    echo "syntax error [A[A."
+    exit
+fi
+
+if [ "$class" == "" ]; then
+    echo "No class provided"
+    exit
+fi
+
+if [ "$elm" == "" ]; then
+    echo "No element provided"
+    exit
+fi
+
+# Only check for default blocklist as pihole list should contain same domains.
+if grep -q "$blocklistRule" "$blocklist"; then 
+        echo "Rule already present"
+    else
+        echo "checking for updates..."
+        git pull origin master && git pull github master
+        
+        if grep -q "$blocklistRule" "$blocklist"; then
+            echo "This rule has already been added before you"
+        else    
+            sed -i '' "s/$versionTag.*/$versionLine/g" $blocklist 
+            
+            printf "$blocklistRule\n" >> "$blocklist"
+            git commit -am "block class ${class} on ${elm} for $domain" && git push origin master && git push github master
+        fi
+fi
+

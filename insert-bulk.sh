643309cb4da16f552aa9aafec5cb6723ab7ab388
ph00lt0 <->
2022-06-20 13:29:36 +0100

    added script to add all tracker partners of yahoo in one go

---
+++ insert-bulk.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+read -p "Enter the domains, comma seperated, you wish to add:" domains
+
+declare blocklist="./blocklist.txt"
+declare piholeBlocklist="./pihole-blocklist.txt"
+
+declare version=$(date +"%Y%m%d")
+declare versionTag="! Version: "
+declare versionLine="! Version: $version"
+
+if [ "$domains" == "" ]; then
+    echo "No domain provided."
+    exit  
+fi
+
+echo "Checking for updates..."
+git pull origin master && git pull github master
+sed -i '' "s/$versionTag.*/$versionLine/g" $blocklist 
+
+echo "Start bulk insertion"
+for domain in ${domains//,/ }; do
+    declare domain=$(echo $domain | sed -E 's/^\s*.*:\/\///g') # remove any https:// or http://.
+    declare domain=$(echo $domain | sed 's:/*$::') # remove any trailing slash.
+
+    declare blocklistRule="||$domain^" 
+    declare allowedRule="@@||$domain^"    
+    declare piholeBlocklistRule="0.0.0.0 $domain"
+    
+    # Only check for default blocklist as pihole list should contain same domains.
+    if grep -q $allowedRule "$blocklist"; then 
+        echo "${domain} whitelisted"
+        return
+    fi
+
+    # Only check for default blocklist as pihole list should contain same domains.
+    if grep -q $blocklistRule "$blocklist"; then 
+        echo "$domain rule already present"
+    else
+        printf "$blocklistRule\n" >> "$blocklist"
+        printf "$piholeBlocklistRule\n" >> "$piholeBlocklist" 
+    fi
+done
+
+git commit -am "added domains in bulk to blocklist" && git push origin master && git push github master

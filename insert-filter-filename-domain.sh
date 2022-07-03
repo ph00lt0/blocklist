af99c90d1a432d235683fa7865abc5bb9dd656e9
ph00lt0 <->
2022-07-04 00:06:48 +0100

    add script to add paths to block files per domain

---
+++ insert-filter-filename-domain.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+declare blocklist="./blocklist.txt"
+
+read -p "Enter the path (full url) of the file: " path
+
+declare version=$(date +"%Y%m%d")
+declare versionTag="! Version: "
+declare versionLine="! Version: $version"
+
+declare path=$(echo $path | sed -E 's/^\s*.*:\/\///g') # remove any https:// or http://.
+declare path=$(echo $path | sed 's:/*$::') # remove any trailing slash.
+
+declare blocklistRule="||${path}^"
+echo $blocklistRule
+
+if [ "$path" == "v" ]; then
+    exit
+fi
+
+if [ "$path" == "[A[A" ]; then
+    echo "syntax error [A[A."
+    exit
+fi
+
+if [ "$path" == "" ]; then
+    echo "No parameter provided"
+    exit
+fi
+
+# Only check for default blocklist as pihole list should contain same domains.
+if grep -q "$blocklistRule" "$blocklist"; then
+        echo "Rule already present"
+    else
+        echo "checking for updates..."
+        git pull origin master && git pull github master
+
+        if grep -q "$blocklistRule" "$blocklist"; then
+            echo "This rule has already been added before you"
+        else
+            sed -i '' "s/$versionTag.*/$versionLine/g" $blocklist
+
+            printf "$blocklistRule\n" >> "$blocklist"
+            git commit -am "block ${path}" && git push origin master && git push github master
+        fi
+fi

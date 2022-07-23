f6986e8efbd24cc3a73df8e6f18ab686a2cdd604
ph00lt0 <->
2022-07-23 18:32:36 +0100

    add unbound and rpz to bulk insert

--- insert-bulk.sh
+++ insert-bulk.sh
@@ -17,10 +17,14 @@ echo "Start bulk insertion"
 for domain in ${domains//,/ }; do
     declare domain=$(echo $domain | sed -E 's/^\s*.*:\/\///g') # remove any https:// or http://.
     declare domain=$(echo $domain | sed 's:/*$::') # remove any trailing slash.
+    declare rpzBlocklist="./rpz-blocklist.txt"
+    declare unboundBlocklist="./unbound-blocklist.txt"
 
     declare blocklistRule="||$domain^"
     declare allowedRule="@@||$domain^"
     declare piholeBlocklistRule="0.0.0.0 $domain"
+    declare rpzBlocklistRule="$domain CNAME ."
+    declare unboundBlocklistRule="local-zone: \"$domain.\" always_null"
 
     # Only check for default blocklist as pihole list should contain same domains.
     if grep -q $allowedRule "$blocklist"; then
@@ -32,9 +36,11 @@ for domain in ${domains//,/ }; do
     if grep -q $blocklistRule "$blocklist"; then
         echo "$domain rule already present"
     else
+        python3 ./ls-insert.py $domain
         printf "$blocklistRule\n" >> "$blocklist"
         printf "$piholeBlocklistRule\n" >> "$piholeBlocklist"
-        python3 ./ls-insert.py $domain
+        printf "$rpzBlocklistRule\n" >> "$rpzBlocklist"
+        printf "$unboundBlocklistRule\n" >> "$unboundBlocklist"
     fi
 done
 git commit -am "added domains in bulk to blocklist" && git push origin master && git push github master

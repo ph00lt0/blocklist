0eb2961995b896075809e2133334c737f37b77a6
T145 <T145@protonmail.com>
2023-09-16 10:31:29 -0700

    fix(badfilter): Cleaned up the script

--- badfilter.py
+++ badfilter.py
@@ -1,29 +1,18 @@
 import sys
 
 def updateLine(line):
-    line = line.replace('@@', '')
-    line = line.replace('^', '^$badfilter')
-    return line
-
-def checkline(line):
-    if line.lstrip().startswith("@@"):
-         return True
-
-    return False
+    return line.replace('@@', '').replace('^', '^$badfilter')
 
 def overwrite(filename='blocklist.txt'):
     with open(filename,'r+') as file:
         lines = file.readlines()
-        count = 0
-        for line in lines:
-            if checkline(line):
+
+        for count, line in enumerate(lines):
+            if line.lstrip().startswith("@@"):
                lines[count] = updateLine(line)
-            count = count + 1
 
         with open(filename,'w') as wfile:
-            for line in lines:
-                wfile.write(line)
-            wfile.close()
+            wfile.writelines(lines)
 
 if __name__ == '__main__':
     overwrite()
